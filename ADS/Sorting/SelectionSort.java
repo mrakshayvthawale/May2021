138ebfafe4985a95822d0d349c92fca2dcad99b3
Kiranwaghmare123 <72081819+Kiranwaghmare123@users.noreply.github.com>
2021-06-15 13:51:00 +0530

    Add files via upload

---
+++ ADS/Sorting/SelectionSort.java
@@ -0,0 +1,44 @@
+
+class SelectionSort
+{
+	void sort(int arr[])
+	{
+		int n = arr.length;
+
+		
+		for (int i = 0; i < n-1; i++)
+		{
+			
+			int min = i;
+			for (int j = i+1; j < n; j++)
+				if (arr[j] < arr[min])
+					min = j;
+
+			
+			int temp = arr[min];
+			arr[min] = arr[i];
+			arr[i] = temp;
+		}
+		
+	}
+
+
+	void display(int arr[])
+	{
+		int n = arr.length;
+		for (int i=0; i<n; ++i)
+			System.out.print(arr[i]+" ");
+		System.out.println();
+	}
+
+
+	public static void main(String args[])
+	{
+		SelectionSort s1 = new SelectionSort();
+		int arr[] = {64,25,12,22,11};
+		s1.sort(arr);
+		System.out.println("Sorted array");
+		s1.display(arr);
+	}
+}
+
